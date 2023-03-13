bf0e4a7e95cd9d9d1a18eed2eebcbaf1916ab6a8
dkrst <dkrst@fesb.hr>
2023-03-13 14:53:18 +0100

    Rjesavan nchannels u validator.py

--- ultralytics/yolo/engine/validator.py
+++ ultralytics/yolo/engine/validator.py
@@ -151,7 +151,7 @@ class BaseValidator:
             self.dataloader = self.dataloader or self.get_dataloader(self.data.get(self.args.split), self.args.batch)
 
             model.eval()
-            model.warmup(imgsz=(1 if pt else self.args.batch, 3, imgsz, imgsz))  # warmup
+            model.warmup(imgsz=(1 if pt else self.args.batch, self.model.ch, imgsz, imgsz))  # warmup
 
         dt = Profile(), Profile(), Profile(), Profile()
         n_batches = len(self.dataloader)
