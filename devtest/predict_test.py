f68e617a8b73e36be76494dd2c2f05e6493d4862
dkrst <dkrst@fesb.hr>
2024-02-23 15:05:40 +0000

    dodatn devtest/predict_test.py - predikcija na npz slikama (ne radi jos na visekanalnim)

---
+++ devtest/predict_test.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+import ultralytics.yolo.data.dataloaders.npyloader as npyloader
+
+
+nloader, ndata = npyloader.create_dataloader('/home/dkrst/GIT/YOLO/data/NPY-new/test/still/', 640, 16,32)nloader, ndata = npyloader.create_dataloader('/home/dkrst/GIT/YOLO/data/NPY-new/test/still/', 640, 16,32)
+
+m, h, w = ndata.load_image(39)
+nim.shape
+
+from ultralytics import YOLO
+model = YOLO('/home/dkrst/GIT/YOLO/yolov8/YOLOv8-Restart/stillRGB_3C/weights/last.pt')
+
+results = model([nim])
+
+#
+# ili
+#
+im = np.load('/home/dkrst/GIT/YOLO/data/NPY-new/test/still/000453_s_000878.npz')['im']
+im.shape
+
+results = model([im])
+
+
