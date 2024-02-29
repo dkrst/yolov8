cf25d161dcdc3dd3ba309705def1596ce383a9d6
dkrst <dkrst@fesb.hr>
2024-02-29 13:24:22 +0000

    Dodan new_cli.py za treniranje

---
+++ devtest/new_cli.py
@@ -0,0 +1,18 @@
+from ultralytics import YOLO
+model = YOLO('/home/dkrst/GIT/YOLO/data/NPY-new/yolov8l_3c.yaml')
+results = model.train(
+    #cfg='/home/dkrst/GIT/YOLO/data/NPY-new/temporal_3C-cfg.yaml',
+    cfg='/home/dkrst/GIT/YOLO/data/NPY-new/stillRGB_3C-cfg.yaml',
+    #cfg='/home/dkrst/GIT/YOLO/data/NPY-new/dist_4C-cfg.yaml',
+    #cfg='/home/dkrst/GIT/YOLO/data/NPY-new/fgr_5C-cfg.yaml',
+    #
+    # data='/home/dkrst/GIT/YOLO/data/NPY-new/stillRGB_3C-data.yaml',
+    # epochs=3,
+    # v5loader=True,
+    # device='cuda:1',
+    # name='TEMPORAL_3C'
+)
+from clearml import Task
+task = Task.current_task()
+task.get_status()
+task.mark_completed()
